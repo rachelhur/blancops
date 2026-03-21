fix: generalization of feature normalization for both environment and train dataset construction

blancops/data_processing/data_processing.py
@@ -255,74 +255,37 @@ def expand_feature_names_for_cyclic_norm(feature_names, cyclical_feature_names):
         ]
     return feature_names
 
-def setup_feature_names(base_global_feature_names, base_bin_feature_names, cyclical_feature_names, nbins, do_cyclical_norm, grid_network):
-    """
-    Returns
-    -------
-    global_feature_names (list): feature names after circular normalization. If grid_network is None, returns [global_feature_names] + [bin_feature_names]
-    bin_feature_names (list): feature names after circular normalization but before adding 'bin_{i}_{feat}' prefixes
-    expanded_global_feature_names
-    """
-    if len(base_bin_feature_names) > 0:
-        prenorm_expanded_bin_feature_names = np.array([ [f'bin_{bin_num}_{bin_feat}'
-                                        for bin_feat in base_bin_feature_names]
-                                        for bin_num in range(nbins) ])
-        prenorm_expanded_bin_feature_names = prenorm_expanded_bin_feature_names.flatten().tolist()
-    else:
-        prenorm_expanded_bin_feature_names = []
+# def setup_feature_names(base_global_feature_names, base_bin_feature_names, cyclical_feature_names, nbins, do_cyclical_norm, grid_network):
+#     """
+#     Returns
+#     -------
+#     global_feature_names (list): feature names after circular normalization. If grid_network is None, returns [global_feature_names] + [bin_feature_names]
+#     bin_feature_names (list): feature names after circular normalization but before adding 'bin_{i}_{feat}' prefixes
+#     expanded_global_feature_names
+#     """
+#     if len(base_bin_feature_names) > 0:
+#         prenorm_expanded_bin_feature_names = np.array([ [f'bin_{bin_num}_{bin_feat}'
+#                                         for bin_feat in base_bin_feature_names]
+#                                         for bin_num in range(nbins) ])
+#         prenorm_expanded_bin_feature_names = prenorm_expanded_bin_feature_names.flatten().tolist()
+#     else:
+#         prenorm_expanded_bin_feature_names = []
 
+#     # Replace cyclical features with their cyclical transforms/normalizations if on  
+#     if do_cyclical_norm:
+#         global_feature_names = expand_feature_names_for_cyclic_norm(base_global_feature_names.copy(), cyclical_feature_names)
+#         bin_feature_names = expand_feature_names_for_cyclic_norm(prenorm_expanded_bin_feature_names.copy(), cyclical_feature_names)
+#     else:
+#         global_feature_names = base_global_feature_names
+#         bin_feature_names = prenorm_expanded_bin_feature_names
+#     return global_feature_names, bin_feature_names, prenorm_expanded_bin_feature_names
+
+def setup_feature_names(base_global_feature_names, base_bin_feature_names, cyclical_feature_names, do_cyclical_norm):
     # Replace cyclical features with their cyclical transforms/normalizations if on  
     if do_cyclical_norm:
         global_feature_names = expand_feature_names_for_cyclic_norm(base_global_feature_names.copy(), cyclical_feature_names)
-        bin_feature_names = expand_feature_names_for_cyclic_norm(prenorm_expanded_bin_feature_names.copy(), cyclical_feature_names)
+        bin_feature_names = expand_feature_names_for_cyclic_norm(base_bin_feature_names.copy(), cyclical_feature_names)
     else:
         global_feature_names = base_global_feature_names
-        bin_feature_names = prenorm_expanded_bin_feature_names
-    return global_feature_names, bin_feature_names, prenorm_expanded_bin_feature_names
-
-def normalize_noncyclic_features(state, 
-                                state_feature_names,
-                                max_norm_feature_names,
-                                ang_distance_norm_feature_names,
-                                do_inverse_norm, do_max_norm, do_ang_distance_norm,
-                                bin_space=None,
-                                fix_nans=True):
-    is_torch = torch.is_tensor(state)
-    # build masks (numpy boolean array)
-    airmass_mask = np.array(['airmass' in feat for feat in state_feature_names], dtype=bool)
-    max_norm_mask = np.array([any(max_feat in feat for max_feat in max_norm_feature_names) for feat in state_feature_names], dtype=bool)
-    ang_distance_mask = np.array([any(dist_feat in feat for dist_feat in ang_distance_norm_feature_names) for feat in state_feature_names], dtype=bool)
-
-    if is_torch:
-        airmass_mask = torch.tensor(airmass_mask, dtype=torch.bool, device=state.device)
-        max_norm_mask = torch.tensor(max_norm_mask, dtype=torch.bool, device=state.device)
-        ang_distance_mask = torch.tensor(ang_distance_mask, dtype=torch.bool, device=state.device)
-
-    do_reshape = False
-
-    if state.ndim == 3: # ie, if is bin states
-        do_reshape = True
-        nrows, nbins, nfeats_per_bin = state.shape
-        if is_torch:
-            state = state.flatten(start_dim=1)
-        else:
-            state = state.reshape(state.shape[0], -1) 
-    # logger.debug(f"airmass mask shape {airmass_mask.shape}")
-    # logger.debug(f"state shape {state.shape}")  
-    if do_inverse_norm:
-        # logger.debug(f"state shape {state.shape}, airmass mask shape {airmass_mask.shape}")
-        state[..., airmass_mask] = 1.0 / state[..., airmass_mask]
-    if do_max_norm:
-        state[..., max_norm_mask] = state[..., max_norm_mask] / (np.pi / 2)
-    if do_ang_distance_norm:
-        # logger.debug(f"DOING ANG DISTANCE NORM for {ang_distance_mask.sum()} number of elements")
-        state[..., ang_distance_mask] = state[..., ang_distance_mask] / np.pi
-    if fix_nans:
-        if is_torch:
-            state[torch.isnan(state)] = 1.2
-        else:
-            state[np.isnan(state)] = 1.2
-    if do_reshape:
-        state = state.reshape(nrows, nbins, nfeats_per_bin)
-
-    return state
+        bin_feature_names = base_bin_feature_names
+    return global_feature_names, bin_feature_names

blancops/data_processing/offline_dataset.py
@@ -376,31 +376,8 @@ class OfflineDataset(torch.utils.data.Dataset):
                     lonlat_no_zen = df.groupby('night').tail(-1)[['ra', 'dec']].values
                 indices = self.hpGrid.ang2idx(lon=lonlat_no_zen[:, 0], lat=lonlat_no_zen[:, 1])
             return indices
-        
         elif binning_method == 'uniform' and bin_space == 'azel':
             raise NotImplementedError
-            az_edges = np.linspace(0, 360, num_bins_1d + 1, dtype=np.float32)
-            # az_centers = az_edges[:-1] + (az_edges[1] - az_edges[0])/2
-            el_edges = np.linspace(0, 90, num_bins_1d + 1, dtype=np.float32)
-            # az_centers = el_edges[:-1] + (el_edges[1] - el_edges[0])/2
-
-            i_x = np.digitize(next_states[:, az_idx], az_edges).astype(np.int32) - 1
-            i_y = np.digitize(next_states[:, el_idx], el_edges).astype(np.int32) - 1
-            bin_ids = i_x + i_y * (num_bins_1d)
-            self.az_edges = az_edges
-            self.el_edges = el_edges
-            
-            id2azel = defaultdict(list)
-            for az, el, bin_id in zip(next_states[:, az_idx], next_states[:, el_idx], bin_ids):
-                id2azel[bin_id].append((az, el))            
-            self.id2azel = dict(sorted(id2azel.items()))
-
-            id2radec = defaultdict(list)
-            for ra, dec, bin_id in zip(df['ra'].values, df['dec'].values, bin_ids):
-                id2radec[bin_id].append((ra, dec))
-            self.id2radec = dict(sorted(id2radec.items()))
-
-            return bin_ids
         else:
             raise NotImplementedError
 
@@ -478,16 +455,9 @@ class OfflineDataset(torch.utils.data.Dataset):
         logger.info(f'Choosing {num_val_nights} nights for validation out of {self.n_nights} nights. Specifically, {val_nights}')
         
         # 2. Track which night each transition belongs to
-        # We must handle the two different ways states are constructed
-        if hasattr(self, 'next_state_idxs') and self.next_state_idxs is not None:
-            # When remove_large_time_diffs is True
-            transition_nights = self._df.iloc[self.next_state_idxs - 1]['night']
-        else:
-            # When remove_large_time_diffs is False
-            transition_nights = self._df.groupby('night')['night'].apply(lambda x: x.iloc[:-1])
+        transition_nights = self._df.iloc[self.next_state_idxs - 1]['night']
             
         # 3. Create boolean mask mapping transitions to the selected val nights
-        # print(transition_nights, val_nights)
         val_mask = np.isin(transition_nights, val_nights)
         
         # 4. Extract the exact indices for train and val subsets