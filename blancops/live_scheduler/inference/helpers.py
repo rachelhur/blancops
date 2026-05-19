97ed164494f3c1e92fc9c8676f3de20fc98f430e
hurrachel <hurrachel@gmail.com>
2026-05-19 09:31:57 -0500

    add `get_above_horizon_targets`

--- blancops/live_scheduler/inference/helpers.py
+++ blancops/live_scheduler/inference/helpers.py
@@ -1,17 +1,20 @@
-import os
-from pathlib import Path
-from turtle import pd
+from blancops.configs.rl_schema import ActionConstraints
+from blancops.data.features.normalizations import StateNormalizer, build_normalizer, build_normalizer_kwargs
+from blancops.environment.live_env import LiveBlancoEnv
 
 import numpy as np
+from astropy.time import Time
+from astropy.coordinates import EarthLocation, SkyCoord, AltAz
+import astropy.units as u
 
-from blancops.data.constants import FILTER2IDX
-from blancops.data.features.normalizations import StateNormalizer, build_normalizer_kwargs, load_normalization_stats
-from blancops.data.lookup import LookupTables
-from blancops.environment.online_env import OnlineBlancoEnv
-from blancops.math import units
-
-def build_env(cfg, trained_model_dir, lookups, sun_el_lim=None, airmass_lim=None, t_start=None, chunk_size: int = 10):
-    z_score_stats, rel_norm_stats = load_normalization_stats(trained_model_dir)
+def build_env(cfg, norm_stats, lookups, telemetry_now):
+    
+    constraints_cfg = ActionConstraints()
+    zscore_stats = norm_stats.get('z_score', {})
+    rel_norm_stats = norm_stats.get('rel_norm', {})
+    
+    global_normalizer = build_normalizer(state_feature_names=cfg.data.global_features, cfg=cfg)
+    bin_normalizer = build_normalizer(state_feature_names=cfg.data.bin_features, cfg=cfg)
     norm_kwargs = build_normalizer_kwargs(cfg.data.norm)
     global_normalizer = StateNormalizer(
         state_feature_names=cfg.data.global_features, 
@@ -21,17 +24,44 @@ def build_env(cfg, trained_model_dir, lookups, sun_el_lim=None, airmass_lim=None
         state_feature_names=cfg.data.bin_features, 
         **norm_kwargs
     )
-    env = OnlineBlancoEnv(
-        cfg=cfg, 
+    env = LiveBlancoEnv(
+        cfg=cfg,
+        constraints_cfg=constraints_cfg,
         lookups=lookups,
         global_normalizer=global_normalizer,
         bin_normalizer=bin_normalizer,
-        z_score_stats=z_score_stats, 
+        z_score_stats=zscore_stats, 
         rel_norm_stats=rel_norm_stats,
-        chunk_size=chunk_size,
-        sun_el_lim=sun_el_lim,
-        airmass_lim=airmass_lim,
-        t_start=t_start
+        telemetry_init=telemetry_now
     )
     return env
 
+def get_above_horizon_targets(obs_time, lookups):
+    blanco_loc = EarthLocation.of_site('ctio')
+    obs_time = '2026-06-24T04:00:00' if obs_time is None else obs_time
+    obs_time = Time(obs_time, scale='utc') 
+
+    # Sky grid
+    ra_bins = lookups.fields.ra * u.rad
+    dec_bins = lookups.fields.dec * u.rad
+    ra_grid, dec_grid = np.meshgrid(ra_bins, dec_bins)
+
+    # Flatten
+    targets = SkyCoord(ra=ra_grid.ravel(), dec=dec_grid.ravel(), frame='icrs')
+
+    # Azel frame
+    altaz_frame = AltAz(obstime=obs_time, location=blanco_loc)
+
+    # Transform targets to azel
+    target_altaz = targets.transform_to(altaz_frame)
+
+    # Mask above horizon
+    is_observable_mask = target_altaz.alt > 0
+
+    observable_grid = is_observable_mask.reshape(ra_grid.shape)
+
+    valid_ra = ra_grid.ravel()[is_observable_mask]
+    valid_dec = dec_grid.ravel()[is_observable_mask]
+
+    print(f"Total targets evaluated: {len(targets)}")
+    print(f"Observable targets at {obs_time.iso}: {np.sum(is_observable_mask)}")
