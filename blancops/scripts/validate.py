6ff1a046530d7f91955c1bda83211b12bea91ca3
hurrachel <hurrachel@gmail.com>
2026-06-13 15:26:20 -0500

    validate: write default ra/dec residual plots

--- blancops/scripts/validate.py
+++ blancops/scripts/validate.py
@@ -11,11 +11,11 @@ from blancops.utils.sys_utils import get_system_device
 
 
 def main():
-    
+
     # ------------------------------
     # ArgParse
     # ------------------------------
-    
+
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('-c', '--cfg_path', type=str, default=None, help="Path to config file. If passed, all other arguments are ignored")
     parser.add_argument('-l', '--logging_level', type=str, default='debug', help='Logging level. Options: info, debug')
@@ -24,16 +24,16 @@ def main():
     parser.add_argument('--save_mollweides', action='store_true', help='Whether to save movie files.')
 
     args = parser.parse_args()
-    
+
     # ------------------------------
     # Load config and device
     # ------------------------------
-    
+
     cfg_path = args.cfg_path
 
     cfg = load_and_validate(cfg_path)
     device = get_system_device()
-    
+
     # ------------------------------
     # Initialize logger
     # ------------------------------
@@ -45,23 +45,25 @@ def main():
         filename='validation.log',
         use_tqdm=True
     )
-    
+
     # ------------------------------
     # Build and run evaluators
     # ------------------------------
     logger.info("Building evaluators...")
     s_eval, m_eval = build_evaluators(cfg, device=device, save_movie=args.save_movies, save_mollweide=args.save_mollweides)
-    
+
     logger.info("Running evaluators...")
     s_eval.run()
     m_eval.run(overwrite=args.force_overwrite)
-    
+
     # ------------------------------
     # Default plots
     # ------------------------------
-    
-    #TODO
-    
-    
-    
-    
+
+    fig, ax = s_eval.plot_2dhist_res('ra', 'dec', normalization='probability')
+    fig.savefig(cfg.outdir / 'ss' / 'ra_vs_dec_res.png')
+    fig, ax =m_eval.plot_2dhist_res('ra', 'dec', normalization='probability')
+    fig.savefig(cfg.outdir / 'ms' / 'ra_vs_dec_res.png')
+
+
+
